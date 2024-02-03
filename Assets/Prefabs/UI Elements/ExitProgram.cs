5bb275f98d0c5b517e5ff99ed6305ad302a93d48
NWAlpha <faketab2669@gmail.com>
2024-02-03 14:08:43 -0600

    Removed Unity's Version Control
    Also added exit button

---
+++ Assets/Prefabs/UI Elements/ExitProgram.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitProgram : MonoBehaviour
+{
+    public void QuitProgram()
+    {
+        Application.Quit();
+    }
+}
